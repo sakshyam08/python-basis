82c8de3843e496e20788fea788038f0fbebaab8e
sakshyam08 <sakshyamkoirala77@.com>
2026-01-21 08:09:08 +0545

    Add python learning folders and examples

---
+++ strings/concatination.py
@@ -0,0 +1,5 @@
+s1="sakshyam"
+s2="koirala"
+s3=s1 + s2
+print("Concatenated String is:",s3)
+
---
+++ strings/substring.py
@@ -0,0 +1,13 @@
+s1="Sakshyam"
+s2="yam"
+
+s3=s2 in s1
+
+if s3==True:
+    print("Given Substring is Found")
+else:
+    print("Given Substring is Not Found")
+
+#Finding index of Substring
+index=s1.index(s2)
+print("Index of Substring is:",index)
